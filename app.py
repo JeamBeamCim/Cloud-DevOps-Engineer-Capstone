62db869df86ad2e6926bf3b57e72bb80312b9c49
GoekhanOezkan <oezkan.goekhan@territory.group>
2023-06-23 14:51:17 +0200

    fix

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "<h1 style='text-align: center;'>Welcome to Udacity Cloud DevOps Engineer Nanodegree Capstone Project!</h1><p>Written by Gökhan Özkan</p>"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
